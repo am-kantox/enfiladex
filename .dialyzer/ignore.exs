bb7afff2ca84d90de61ab71e7bae8b89085d8c03
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2024-01-10 07:54:33 +0100

    Make dialyzer happy

--- .dialyzer/ignore.exs
+++ .dialyzer/ignore.exs
@@ -1,6 +1,6 @@
 [
   {"src/enfiladex.erl", :pattern_match},
   {"src/enfiladex.erl", :unused_fun},
-  {"lib/enfiladex.ex", :no_return, 56},
-  {"lib/enfiladex.ex", :pattern_match, 59}
+  {"lib/enfiladex.ex", :no_return, 58},
+  {"lib/enfiladex.ex", :pattern_match, 61}
 ]
